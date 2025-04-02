7426a10ce81a098956d91c6c974533076485bbcf
Jailton <jailtonps@msn.com>
2025-04-01 22:46:31 -0300

    Contador

---
+++ DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,6 @@
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);  // Passa a mensagem para o construtor da classe Exception
+    }
+}
